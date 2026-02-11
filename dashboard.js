5d664aef167a68111981bf06d296754e6bdf9d0f
Jeet9840 <jjinadra606@rku.ac.in>
2026-02-12 01:00:28 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,27 @@
+function goBalance(){
+  window.location.href = "balance.html";
+}
+
+function goTransfer(){
+  window.location.href = "transfer.html";
+}
+
+function goTransactions(){
+  window.location.href = "transactions.html";
+}
+
+function goLoans(){
+  window.location.href = "loan.html";
+}
+
+function goIPO(){
+  window.location.href = "ipo.html";
+}
+
+function goProfile(){
+  window.location.href = "profile.html";
+}
+
+function toggleSidebar(){
+  document.getElementById("sidebar").classList.toggle("closed");
+}
